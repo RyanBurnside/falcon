5ab39b032f9f467582455b3d22319bdc926781c1
RyanBurnside <pixeloutlaw@gmail.com>
2020-06-02 22:14:03 -0600

    Working with prefix field now...

--- falcon.lisp
+++ falcon.lisp
@@ -9,28 +9,41 @@
 ;; Regex filtering list
 ;; arrg my foot is bleeding
 
+;; BUG Windows version of Tk puts / marks in filepaths from file chooser
 
 (in-package #:falcon)
 
-(defparameter *master-list*
-  (loop :for i :from 0 :to 1000
-     :collecting (format nil "DUMMY ~a" i)))
-
+(defparameter *master-list* nil)
 (defparameter *flags* nil)
 (defparameter *buttons-across* 7)
-
 
 (defun notify (message title)
   (message-box message title "ok" "info"))
 
 (defun populate-from-directory (&optional (program-prefix-string ""))
   (let* ((d (choose-directory :title "Choose a directory to populate files from"))
-	 (files (directory (make-pathname :directory 
+	 (is-windows (numberp (search "WIN" (string-upcase (software-type)))))
+	 (files nil))
+
+    (format *standard-output* "Directory is: ~a" d)
+    
+    (when is-windows
+      (setf d (substitute #\\ #\/ d)))
+
+    (setf files (uiop:directory-files (make-pathname :directory 
 					  `(:absolute ,d)
-					  :name :wild :type :wild))))
+					  :name :wild :type :wild)))
+    (uiop:directory-files "./")
+    (format *standard-output* "Directory is now: ~a" d)
+    
     (setf *master-list*
 	  (loop :for i :in files :collecting
-	     (list (file-namestring i) (format nil "~a \"~a\"" program-prefix-string i))))))
+	     (list (file-namestring i)
+		   (format nil "~a \"~a\"" program-prefix-string i))))
+    
+    (print files)
+    (print *master-list*)
+    (finish-output)))
 
 (defparameter *light-color* "#fff7c5")
 (defparameter *middle-color* "#d48626")
@@ -41,9 +54,9 @@
   "Send wish some direct commands since ltk doesn't work with styles"
   
   (send-wish "ttk::style theme use default")
-  ;(send-wish (format nil "ttk::style configure TButton -background \"~a\" -foreground \"~a\" -borderwidth 3" *very-dark-color* *middle-color*))
-  ;(send-wish "ttk::style configure TMenu -background \"#d48626\" -foreground \"#fff7c5\"")
-  ;(send-wish "ttk::style configure TFrame -background \"#fff7c5\"")
+  ;;(send-wish (format nil "ttk::style configure TButton -background \"~a\" -foreground \"~a\" -borderwidth 3" *very-dark-color* *middle-color*))
+  ;;(send-wish "ttk::style configure TMenu -background \"#d48626\" -foreground \"#fff7c5\"")
+  ;;(send-wish "ttk::style configure TFrame -background \"#fff7c5\"")
   
   )
 
@@ -52,8 +65,7 @@
   (with-ltk ()
     (wm-title *tk* "Falcon Button Tray")
     (set-geometry *tk* 640 480 0 0)
-    (style-setup)
-    (populate-from-directory "mednafen")
+    (style-setup)    (populate-from-directory)
     (let* ((menu (make-instance 'menubar :master *tk*))
 	   (file-menu (make-instance 'menu :master menu :name "file" :text "File"))
 	   (file-menu-open-directory
@@ -75,7 +87,7 @@
 	   (path-hull (make-instance 'frame :master tab-frame))
 	   (path-label (make-instance 'label :master path-hull :text "Populate From: "))
 	   (path-field (make-instance 'entry :master path-hull :text "."))
-	   (path-button (make-instance 'button :master path-hull :text "..." :width 3))
+	   (path-button (make-instance 'button :master path-hull :text "..." :width 3 :command #'populate-from-directory))
 	   
 	   (run-parallel-hull (make-instance 'frame :master tab-frame))
 	   (run-parallel-label (make-instance 'label :master run-parallel-hull :text "Spawn New Process: "))
@@ -117,7 +129,11 @@
 	   (grid (make-instance 'button
 				:master (interior pw)
 				:text name
-				:command (lambda() (asdf::run-program command)))
+				:command (lambda() (asdf::run-program
+						    (concatenate 'string
+								 (text prefix-field)
+								 " "
+								 command))))
 		 row column :sticky "EW" :padx 1 :pady 1))
 	 (cond ((>= column (1- *buttons-across*))
 		(setf column 0)
@@ -136,16 +152,14 @@
     (setf *master-list* descs)))
 
 (defun main ()
-					; (format *standard-output* "Recieved Arguments: ~a" sb-ext:*posix-argv*)
+  ;; (format *standard-output* "Recieved Arguments: ~a" sb-ext:*posix-argv*)
   ;; This doesn't QUITE work because it'll read stdin all the time.
   ;; you prolly need a command line argument like '--' or something to indicate
   ;; it is ok to read stdin--otherwise it would be ignored.
-
-					; (setf *flags* (apply-argv:parse-argv sb-ext:*posix-argv*))
-  (setf *master-list*
-	(loop :for (a b) :on *master-list* :by #'cddr collect (list a b)))
-					; (format t "~a" *flags*)
-					;
+  ;; (setf *flags* (apply-argv:parse-argv sb-ext:*posix-argv*))
+  ;; (setf *master-list*
+  ;; (loop :for (a b) :on *master-list* :by #'cddr collect (list a b)))
+  ;; (format t "~a" *flags*)
   
   (start-gui))
 
