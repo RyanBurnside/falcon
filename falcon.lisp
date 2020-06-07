4f62d2ed8608adf7772c90aa21d88d288280d302
RyanBurnside <pixeloutlaw@gmail.com>
2020-06-06 22:01:43 -0600

    Various fixes...

--- falcon.lisp
+++ falcon.lisp
@@ -1,13 +1,8 @@
 ;;;; falcon.lisp
 
 ;; TODO List
-;; Multiple folders
-;; Prefix out of master list and in to single field
-;; Nasty OS dependent filepath fixes (spaces, seperators, escape characters YUCK!)
-;; Call with directory(s) and prefix
 ;; Favorites tab populated
 ;; Regex filtering list
-;; arrg my foot is bleeding
 
 ;; BUG Windows version of Tk puts / marks in filepaths from file chooser
 
@@ -15,35 +10,24 @@
 
 (defparameter *master-list* nil)
 (defparameter *flags* nil)
-(defparameter *buttons-across* 7)
+(defparameter *buttons-across* 1)
+
+;; The following are used to manipulate the button tray outside Tk
+(defparameter *button-hull* nil)
+(defparameter *prefix-field* nil)
 
 (defun notify (message title)
   (message-box message title "ok" "info"))
 
 (defun populate-from-directory (&optional (program-prefix-string ""))
   (let* ((d (choose-directory :title "Choose a directory to populate files from"))
-	 (is-windows (numberp (search "WIN" (string-upcase (software-type)))))
-	 (files nil))
+         (files nil))
 
-    (format *standard-output* "Directory is: ~a" d)
-    
-    (when is-windows
-      (setf d (substitute #\\ #\/ d)))
-
-    (setf files (uiop:directory-files (make-pathname :directory 
-					  `(:absolute ,d)
-					  :name :wild :type :wild)))
-    (uiop:directory-files "./")
-    (format *standard-output* "Directory is now: ~a" d)
-    
-    (setf *master-list*
-	  (loop :for i :in files :collecting
-	     (list (file-namestring i)
-		   (format nil "~a \"~a\"" program-prefix-string i))))
-    
-    (print files)
-    (print *master-list*)
-    (finish-output)))
+    (setf *master-list* (uiop:directory-files (make-pathname :directory
+                                                             `(:absolute ,d)
+                                                             :name :wild :type :wild)))
+    (finish-output)
+    (refresh-tray)))
 
 (defparameter *light-color* "#fff7c5")
 (defparameter *middle-color* "#d48626")
@@ -52,52 +36,76 @@
 
 (defun style-setup ()
   "Send wish some direct commands since ltk doesn't work with styles"
-  
+
   (send-wish "ttk::style theme use default")
   ;;(send-wish (format nil "ttk::style configure TButton -background \"~a\" -foreground \"~a\" -borderwidth 3" *very-dark-color* *middle-color*))
   ;;(send-wish "ttk::style configure TMenu -background \"#d48626\" -foreground \"#fff7c5\"")
   ;;(send-wish "ttk::style configure TFrame -background \"#fff7c5\"")
-  
+  (send-wish "ttk::style configure TButton -background \"#fff7c5\"")
+
   )
 
+(defun refresh-tray (&optional (kill-children nil))
+  (if (or (null *button-hull*)
+          (null *prefix-field*))
+      (return-from refresh-tray))
+
+  (let ((row 0)
+        (column 0))
+    (loop :for path :in *master-list* :do
+       (let ((path path))
+         (grid (make-instance 'button
+                              :master *button-hull*
+                              :text (file-namestring path)
+                              :command (lambda() (uiop:launch-program 
+                                                  (format nil "\"~a\" \"~a\"" ;Windows/Linux hack "command" "full path with spaces"
+                                                          (text *prefix-field*)
+                                                          (namestring path)))))
+               row column :sticky "EW" :padx 1 :pady 1))
+       (cond ((>= column (1- *buttons-across*))
+              (setf column 0)
+              (incf row))
+             (t
+              (incf column))))))
 
 (defun start-gui ()
   (with-ltk ()
     (wm-title *tk* "Falcon Button Tray")
     (set-geometry *tk* 640 480 0 0)
-    (style-setup)    (populate-from-directory)
+    (style-setup)
     (let* ((menu (make-instance 'menubar :master *tk*))
-	   (file-menu (make-instance 'menu :master menu :name "file" :text "File"))
-	   (file-menu-open-directory
-	    (make-instance 'menubutton
-			   :master file-menu
-			   :text "Open Directory"))
-	   (file-menu-quit
-	    (make-instance 'menubutton
-			   :master file-menu
-			   :text "Quit"))
-	   (about-menu (make-instance 'menu :master menu :name "about" :text "Aboot"))
-	   (nb (make-instance 'notebook :master *tk*))
-	   (tab-frame (make-instance 'frame :master nb :name "tab_frame"))
-	   (prefix-hull (make-instance 'frame :master tab-frame))
-	   (prefix-label (make-instance 'label :master prefix-hull :text "Call with prefix: "))
-	   (prefix-field (make-instance 'entry :master prefix-hull :text ""))
-	   (prefix-button (make-instance 'button :master prefix-hull :text "..." :width 3))
+           (file-menu (make-instance 'menu :master menu :name "file" :text "File"))
+           (file-menu-open-directory
+            (make-instance 'menubutton
+                           :master file-menu
+                           :text "Open Directory"))
+           (file-menu-quit
+            (make-instance 'menubutton
+                           :master file-menu
+                           :text "Quit"))
+           (about-menu (make-instance 'menu :master menu :name "about" :text "Aboot"))
+           (nb (make-instance 'notebook :master *tk*))
+           (tab-frame (make-instance 'frame :master nb :name "tab_frame"))
+           (prefix-hull (make-instance 'frame :master tab-frame))
+           (prefix-label (make-instance 'label :master prefix-hull :text "Call with prefix: "))
+           (prefix-field (make-instance 'entry :master prefix-hull :text ""))
+           (prefix-button (make-instance 'button :master prefix-hull :text "..." :width 3))
 
-	   (path-hull (make-instance 'frame :master tab-frame))
-	   (path-label (make-instance 'label :master path-hull :text "Populate From: "))
-	   (path-field (make-instance 'entry :master path-hull :text "."))
-	   (path-button (make-instance 'button :master path-hull :text "..." :width 3 :command #'populate-from-directory))
-	   
-	   (run-parallel-hull (make-instance 'frame :master tab-frame))
-	   (run-parallel-label (make-instance 'label :master run-parallel-hull :text "Spawn New Process: "))
-	   (run-parallel-check (make-instance 'check-button :master run-parallel-hull))
+           (path-hull (make-instance 'frame :master tab-frame))
+           (path-label (make-instance 'label :master path-hull :text "Populate From: "))
+           (path-field (make-instance 'entry :master path-hull :text "."))
+           (path-button (make-instance 'button :master path-hull :text "+" :width 3 :command #'populate-from-directory))
 
-	   (refresh-button (make-instance 'button :master tab-frame :text "Refresh" :command #'populate-from-directory))	   
-	   (pw (make-instance 'scrolled-frame :master tab-frame :name "scrolled_frame"))
-	   (pw2 (make-instance 'scrolled-frame :master nb :name "scrolled_frame2"))
-	   (row 0)
-	   (column 0))
+           (run-parallel-hull (make-instance 'frame :master tab-frame))
+           (run-parallel-label (make-instance 'label :master run-parallel-hull :text "Spawn New Process: "))
+           (run-parallel-check (make-instance 'check-button :master run-parallel-hull))
+
+           (refresh-button (make-instance 'button :master tab-frame :text "Refresh" :command #'populate-from-directory))
+           (pw (make-instance 'scrolled-frame :master tab-frame :name "scrolled_frame"))
+           (pw2 (make-instance 'scrolled-frame :master nb :name "scrolled_frame2")))
+
+      (setf *prefix-field* prefix-field)
+      (setf *button-hull* (interior pw))
 
       (pack nb :fill "both" :expand t)
       (pack prefix-hull :anchor "NW" :side "top")
@@ -109,47 +117,20 @@
       (pack path-label :side "left")
       (pack path-field :side "left")
       (pack path-button :side "left")
-      
+
       (pack run-parallel-hull :anchor "NW" :side "top")
       (pack run-parallel-label :side "left")
       (pack run-parallel-check :side "left")
 
       (pack refresh-button :anchor "NW" :side "top")
-      
+
       (pack pw :fill "both" :anchor "NW" :side "top" :expand t)
       (notebook-enable-traversal nb)
       (notebook-add nb tab-frame :text "Output")
       (notebook-add nb pw2 :text "Favorites")
+      (refresh-tray)
+      )))
 
-
-      
-      (loop :for (name command) :in *master-list* :do
-	 (let ((name name)
-	       (command command))
-	   (grid (make-instance 'button
-				:master (interior pw)
-				:text name
-				:command (lambda() (asdf::run-program
-						    (concatenate 'string
-								 (text prefix-field)
-								 " "
-								 command))))
-		 row column :sticky "EW" :padx 1 :pady 1))
-	 (cond ((>= column (1- *buttons-across*))
-		(setf column 0)
-		(incf row))
-	       (t
-		(incf column)))))))
-
-(defun read-piped-file ()
-  (let* ((strings
-	  (loop :for str = (read *standard-input* nil :eof)
-	     :until (eq str :eof)
-	     :collect str))
-	 (descs
-	  (loop :for (button-name command-string) :on strings :by #'cddr
-	     :collect (list button-name command-string))))
-    (setf *master-list* descs)))
 
 (defun main ()
   ;; (format *standard-output* "Recieved Arguments: ~a" sb-ext:*posix-argv*)
@@ -160,7 +141,5 @@
   ;; (setf *master-list*
   ;; (loop :for (a b) :on *master-list* :by #'cddr collect (list a b)))
   ;; (format t "~a" *flags*)
-  
+
   (start-gui))
-
-
