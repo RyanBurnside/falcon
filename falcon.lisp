e232d832d61eac12a4701f233f4dbbf9823ea2cb
RyanBurnside <pixeloutlaw@gmail.com>
2020-06-15 21:26:42 -0600

    Added in button refresh.

--- falcon.lisp
+++ falcon.lisp
@@ -3,10 +3,7 @@
 ;; TODO List
 ;; Favorites tab populated
 ;; Regex filtering list
-
-;; Make a function to generate a new button pane given the parent.
-;; This way you can DELETE it, then recreate it.
-
+;; Bind program selector result to prefix field
 
 ;; BUG Windows version of Tk puts / marks in filepaths from file chooser
 
@@ -20,17 +17,36 @@
 (defparameter *button-hull* nil)
 (defparameter *prefix-field* nil)
 
+(defun tk-parent (w)
+  "Returns a Wish understood representation of the CLOS object parent"
+  (let* ((old-path (widget-path w))
+         (parent-path (subseq old-path 0 (search "." old-path :from-end t))))
+    parent-path))
+
+(defun tk-destroy-widget (w)
+  "Given a CLOS object destroys it on the Wish side"
+  (send-wish (concatenate 'string "destroy " (widget-path w)))
+  (setf w NIL))
+
+(defun tk-destroy-children (w)
+  "Given a CLOS object destroys the children on the Wish side"
+  (let ((command-string
+         "foreach w [winfo children \"~a\"] { destroy $w }"))
+    (send-wish (format nil command-string (widget-path w)))))
+
 (defun notify (message title)
   (message-box message title "ok" "info"))
 
 (defun populate-from-directory (&optional (program-prefix-string ""))
+  (unless *button-hull*
+    (tk-destroy-children *button-hull*))
+
   (let* ((d (choose-directory :title "Choose a directory to populate files from"))
          (files nil))
 
     (setf *master-list* (uiop:directory-files (make-pathname :directory
                                                              (list :absolute d))))
-    (dolist (i *master-list*) (print i))
-    (finish-output)
+
     (refresh-tray)))
 
 (defparameter *light-color* "#fff7c5")
@@ -38,18 +54,20 @@
 (defparameter *dark-color* "#6d2317")
 (defparameter *very-dark-color* "#1a0500")
 
-(defun style-setup ()
+(defun style-setup () ;; Messing with color a bit, I hope for sickly medical pastels eventually
   "Send wish some direct commands since ltk doesn't work with styles"
 
   (send-wish "ttk::style theme use default")
   ;;(send-wish (format nil "ttk::style configure TButton -background \"~a\" -foreground \"~a\" -borderwidth 3" *very-dark-color* *middle-color*))
   ;;(send-wish "ttk::style configure TMenu -background \"#d48626\" -foreground \"#fff7c5\"")
   ;;(send-wish "ttk::style configure TFrame -background \"#fff7c5\"")
-  (send-wish "ttk::style configure TButton -background \"#fff7c5\"")
+  (send-wish "ttk::style configure TButton -background \"#fff7c5\""))
 
-  )
 
-(defun refresh-tray (&optional (kill-children nil))
+;; TODO MAKE THIS SIMPLY GENERATE AN INSTANCE OF THE TRAY!
+;; MAKE A FUNCTION TO DELETE THE ORIGINAL AND REBIND INSTEAD!!!
+(defun refresh-tray ()
+  "Destroys then repopulates file launch buttons"
   (if (or (null *button-hull*)
           (null *prefix-field*))
       (return-from refresh-tray))
@@ -57,7 +75,7 @@
   (let ((row 0)
         (column 0))
     (loop :for path :in *master-list* :do
-       (let ((path path))
+       (let ((path path)) ; We force eval of path when binding to the button's action
          (grid (make-instance 'button
                               :master *button-hull*
                               :text (uiop:native-namestring (file-namestring path))
@@ -71,6 +89,7 @@
               (incf row))
              (t
               (incf column))))))
+
 
 (defun start-gui ()
   (with-ltk ()
@@ -96,15 +115,14 @@
            (prefix-button (make-instance 'button :master prefix-hull :text "..." :width 3))
 
            (path-hull (make-instance 'frame :master tab-frame))
-           (path-label (make-instance 'label :master path-hull :text "Populate From: "))
-           (path-field (make-instance 'entry :master path-hull :text "."))
+           (path-label (make-instance 'label :master path-hull :text "Add From Directory: "))
            (path-button (make-instance 'button :master path-hull :text "+" :width 3 :command #'populate-from-directory))
 
            (run-parallel-hull (make-instance 'frame :master tab-frame))
            (run-parallel-label (make-instance 'label :master run-parallel-hull :text "Spawn New Process: "))
            (run-parallel-check (make-instance 'check-button :master run-parallel-hull))
 
-           (refresh-button (make-instance 'button :master tab-frame :text "Clear All"))
+           (refresh-button (make-instance 'button :master tab-frame :text "Clear All" :command (lambda () (tk-destroy-children *button-hull*))))
            (pw (make-instance 'scrolled-frame :master tab-frame :name "scrolled_frame"))
            (pw2 (make-instance 'scrolled-frame :master nb :name "scrolled_frame2")))
 
@@ -119,7 +137,6 @@
 
       (pack path-hull :anchor "NW" :side "top")
       (pack path-label :side "left")
-      (pack path-field :side "left")
       (pack path-button :side "left")
 
       (pack run-parallel-hull :anchor "NW" :side "top")
@@ -146,6 +163,4 @@
   ;; (loop :for (a b) :on *master-list* :by #'cddr collect (list a b)))
   ;; (format t "~a" *flags*)
 
-  (start-gui)
-  (setf *master-list* nil)
-  )
+  (start-gui))
